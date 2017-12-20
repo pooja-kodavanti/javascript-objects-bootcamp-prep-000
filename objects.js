f1ce23da38feb610ac9546ae82895be231fc2a7a
Pooja Kodavanti <pkodavan@gmail.com>
2017-12-20 17:16:25 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -9,10 +9,9 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, {[artistName]: songTitle})
 }
 
-const artistName = 'Kanye'
 function removeFromPlaylist(playlist, artistName) {
-  var playlist = {[artistName]: "Gold Digger"}
+  var playlist = {[artistName]: songTitle}
   var artistName = 'Kanye'
+  var songTitle = "Gold Digger"
   delete playlist.'Kanye'
-  return playlist
 }
