ff82a8581ab6e0362773c8d82a35c169f9314f63
Pooja Kodavanti <pkodavan@gmail.com>
2017-12-20 16:47:39 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,11 @@
-var playlist = {cher: "believe"}
+var playlist = {};
 
 function updatePlaylist(playlist, artistName, songTitle) {
   var artistName = 'Phil Ochs'
   var songTitle = "Here's to the State of Mississippi"
   return Object.assign({}, playlist, {[artistName]: songTitle]})
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  var artistName = ''
 }
