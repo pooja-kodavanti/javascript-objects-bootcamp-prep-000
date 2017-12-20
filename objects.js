75a3d0dfdf004f6e4ff2bee76349335f7e372add
Pooja Kodavanti <pkodavan@gmail.com>
2017-12-20 16:44:24 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,7 @@
 var playlist = {cher: "believe"}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  Object.assign({}, playlist, {[artistName]: songTitle]})
+  var artistName = 'Phil Ochs'
+  var songTitle = "Here's to the State of Mississippi"
+}
